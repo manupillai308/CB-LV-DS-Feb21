edcbcce93c62f6369e90bd88b33905979452a644
Manu S Pillai <manupillai308@gmail.com>
2021-08-12 02:10:27 +0530

    Session 37

--- DS/NN/run.py
+++ DS/NN/run.py
@@ -1,19 +1,24 @@
 from model import Sequential
-from layer import Dense
+from layer import Dense, Conv2D, MaxPool2D, Flatten
 from loss import BinaryCrossEntropy
 from activation import Sigmoid
 from optimizer import GradientDescentOptimizer
 
 if __name__ == "__main__":
+    from sklearn.datasets import load_digits
+    data = load_digits(n_class=2)
+    X, y = data['data'].reshape(-1,8,8,1)/16, data['target'].reshape(-1,1)
+
     model = Sequential()
-    model.add(Dense, units=3, activation=Sigmoid(), input_size=2)
-    model.add(Dense, units=2, activation=Sigmoid())
+    model.add(Conv2D, ksize=3, stride=1, activation=Sigmoid(), input_size=(8,8,1), filters=1, padding=0)
+    model.add(MaxPool2D, ksize=2, stride=1, padding=0)
+    model.add(Conv2D, ksize=2, stride=1, activation=Sigmoid(), filters=1, padding=0)
+    model.add(Flatten)
     model.add(Dense, units=1, activation=Sigmoid())
-    model.compile(BinaryCrossEntropy())
     model.summary()
 
-    from sklearn.datasets import make_gaussian_quantiles
-    X, y = make_gaussian_quantiles(n_samples=200, n_classes=2)
-    y = y.reshape(-1,1)
-    print("Loss", model.evaluate(X, y)[0])
-    model.fit(X, y, n_epochs=1000, batch_size=1, learning_rate=0.003, optimizer=GradientDescentOptimizer(), verbose=1)
+    model.compile(BinaryCrossEntropy())
+
+    print("Initial Loss", model.evaluate(X, y)[0])
+    model.fit(X, y, n_epochs=100, batch_size=300, learning_rate=0.003, optimizer=GradientDescentOptimizer(), verbose=1)
+    print("Final Loss", model.evaluate(X, y)[0])
